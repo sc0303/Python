fdee884552e2655e188fcf91c65c20767d3cdb93
SunChuan <sunchuan@bestpay.com.cn>
2016-05-03 15:39:11 +0800

    v20160503_v1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import py2exe
+import sys
+includes = ["encodings", "encodings.*"]
+sys.argv.append("py2exe")
+options = {"py2exe":  { "bundle_files": 1 }
+                }
+setup(options = options,
+      zipfile=None,
+      console = [{"script":'base.py', 'icon_resources':[(1, 'C:/Users/Alance/PycharmProjects/Python/logo.ico')]}],)
+
+
