75b1abca2169762ef0c2ff26c342c4937ca95949
SunChuan_Home <sc0303@126.com>
2016-05-30 00:01:11 +0800

    v20160529_home

--- decisionTree.py
+++ decisionTree.py
@@ -3,44 +3,46 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from math import log
-
-
-#########################计算熵指#####################33
-def calcShannonEnt(dataset):
-    dataLen = len(dataset)
-    labelCounts = {}
-    for vec in dataset:
-        currentValue = str(vec[-1])
-        if currentValue not in labelCounts:
-            labelCounts[currentValue] = 0
-        labelCounts[currentValue] = labelCounts[currentValue] + 1
-
-    shannoEnt = 0
-    for key in labelCounts:
-        pro = float(labelCounts[key]) / dataLen
-        shannoEnt = shannoEnt - pro * log(pro, 2)
-    return shannoEnt
-
-
-#############################创建dataset#################
-def createDataSet():
-    dataSet = [['1', '1', 'yes'], ['1', '0', 'no'], ['1', '1', 'yes'], ['0', '1', 'no'], ['0', '1', 'no']]
-    labels = ['no surfacing', 'flippers']
-
-    return dataSet, labels
-
-
-############################分割数据####################
-def splitData(dataSet, axis, value):
-    resultData = []
-    for vector in dataSet:
-        if vector[axis] == value:
-            reducedVector = vector[:axis]
-            reducedVector.extend(vector[axis + 1:])
-            resultData.append(reducedVector)
-    return resultData
-
-
+from sklearn import tree
+from sklearn.datasets import load_iris
+from sklearn.externals.six import StringIO
+import pydot
+# #########################计算熵指#####################33
+# def calcShannonEnt(dataset):
+#     dataLen = len(dataset)
+#     labelCounts = {}
+#     for vec in dataset:
+#         currentValue = str(vec[-1])
+#         if currentValue not in labelCounts:
+#             labelCounts[currentValue] = 0
+#         labelCounts[currentValue] = labelCounts[currentValue] + 1
+#
+#     shannoEnt = 0
+#     for key in labelCounts:
+#         pro = float(labelCounts[key]) / dataLen
+#         shannoEnt = shannoEnt - pro * log(pro, 2)
+#     return shannoEnt
+#
+#
+# #############################创建dataset#################
+# def createDataSet():
+#     dataSet = [['1', '1', 'yes'], ['1', '0', 'no'], ['1', '1', 'yes'], ['0', '1', 'no'], ['0', '1', 'no']]
+#     labels = ['no surfacing', 'flippers']
+#
+#     return dataSet, labels
+#
+#
+# ############################分割数据####################
+# def splitData(dataSet, axis, value):
+#     resultData = []
+#     for vector in dataSet:
+#         if vector[axis] == value:
+#             reducedVector = vector[:axis]
+#             reducedVector.extend(vector[axis + 1:])
+#             resultData.append(reducedVector)
+#     return resultData
+#
+#
 #########################挑选最大的熵值#########################
 def chooseBest(dataSet):
     numFeatures = len(dataSet[0]) - 1
@@ -61,32 +63,47 @@ def chooseBest(dataSet):
 
 
 ######################创建绘图###########################
-decisionNode = dict(boxstyle="sawtooth", fc="0.8")
-leafNode = dict(boxstyle="round4", fc="0.8")
-arrow_args = dict(arrowstyle="<-")
-
-
-def plotNode(nodeTxt, centerPt, parentPt, nodeType):
-    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
-                            xytext=centerPt, textcoords='axes fraction',
-                            va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
-
-
+# decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+# leafNode = dict(boxstyle="round4", fc="0.8")
+# arrow_args = dict(arrowstyle="<-")
+#
+#
+# def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+#     plt.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
+#                  xytext=centerPt, textcoords='axes fraction',
+#                  va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+#
+#
 def createPlot():
     fig = plt.figure(1,facecolor='white')
     fig.clf()
-    createPlot.ax1 = plt.subplots(111,frameon = 'false')
+    # ax1 = plt.subplots(111,frameon = 'false')
     plotNode('a decision node', (0.5, 0.1),(0.1, 0.5),decisionNode)
     plotNode('a leaf node',(0.8, 0.1),(0.3, 0.8),leafNode)
     plt.show()
+#
+# createPlot()
 
-createPlot()
+def readFile(dateSet):
+    with open(dateSet) as f:
+        frTmp = f.readlines()
+        fr = [lineTmp.strip().split('\t') for lineTmp in frTmp]
+        frInput = [frInputTmp[0:3] for frInputTmp in fr]
+        frOutput = [frOutputTmp[4:] for frOutputTmp in fr]
+        return frInput, frOutput
 
-
-
-myData, myLabels = createDataSet()
-
-print(chooseBest(myData))
+input, output = readFile('C:/Users/Alance/PycharmProjects/machinelearninginaction/Ch03/lenses.txt')
+iris = load_iris()
+myClf = tree.DecisionTreeClassifier()
+myClf.fit(iris.data, iris.target)
+data = StringIO()
+tree.export_graphviz(myClf,out_file = data)
+print(data.getvalue())
+graph = pydot.graph_from_dot_data(data.getvalue())
+graph.write_pdf('C:/Users/Alance/PycharmProjects/machinelearninginaction/Ch03/dt.pdf')
+# myData, myLabels = createDataSet()
+#
+# print(chooseBest(myData))
 
 
 
