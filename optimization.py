f8c21d0145c3542ed58708ffe19e3fc107ccd199
SunChuan <sunchuan@bestpay.com.cn>
2016-06-24 15:44:25 +0800

    v20160624

---
+++ optimization.py
@@ -0,0 +1,202 @@
+# Write by SunChuan on 20160622
+import math
+import random
+import time
+
+people = [('Seymour', 'BOS'),
+          ('Franny', 'DAL'),
+          ('Zooey', 'CAK'),
+          ('Walt', 'MIA'),
+          ('Buddy', 'ORD'),
+          ('Les', 'OMA')]
+# Laguardia
+destination = 'LGA'
+
+flight = {}
+for line in open('D:/DataMining/machinelearninginaction/PCI_Code Folder/chapter5/schedule.txt'):
+    origin, destination_tmp, start_time, arrive_time, price = line.strip().split(',')
+    flight.setdefault((origin, destination_tmp), [])
+    flight[(origin, destination_tmp)].append((start_time, arrive_time, int(price)))  # 注意append的用法
+
+
+def get_minutes(t):
+    x = time.strptime(t, '%H:%M')
+    return x[3] * 60 + x[4]
+
+
+def print_schedule(r):
+    for d in range(int(len(r) / 2)):
+        name = people[d][0]
+        origin = people[d][1]
+        out = flight[origin, destination][r[d * 2]]  # 列表使用起来太灵活了！！！！
+        back = flight[origin, destination][r[d * 2 + 1]]
+        print('%10s%10s %5s - %5s $%3s %5s - %5s $%3s' % (
+            name, origin, out[0], out[1], out[2], back[0], back[1], back[2]))  # 注意多个输出如何使用
+
+
+def schedulecost(sol):
+    total_price = 0
+    last_arrivarl = 0
+    earliest_dep = 24 * 60
+    for i in range(int(len(sol) / 2)):
+        origin = people[i][1]
+        # 得到往返航班
+        # print(sol[i * 2])
+        try:
+            out = flight[origin, destination][sol[i * 2]]
+            back = flight[origin, destination][sol[i * 2 + 1]]
+        except:
+            print(sol[i * 2] +1)
+            print(len(flight[0]))
+            print('hello')
+
+        total_price += out[2] + back[2]
+        if last_arrivarl < get_minutes(out[1]): last_arrivarl = get_minutes(out[1])
+        if earliest_dep > get_minutes(back[0]): earliest_dep = get_minutes(back[0])
+    total_wait = 0
+
+    for i in range(int(len(sol) / 2)):
+        origin = people[i][1]
+        out = flight[origin, destination][sol[i * 2]]
+        back = flight[origin, destination][sol[i * 2 + 1]]
+        total_wait = total_wait + last_arrivarl - get_minutes(out[1]) + get_minutes(back[0]) - earliest_dep
+
+    if last_arrivarl > earliest_dep: total_price = total_price + 50
+    return total_wait + total_price
+
+
+def random_optimize(random_num, costf):
+    best_cost = 99999999
+    best_r = None
+    random_input = [random.randint(random_num[i][0], random_num[i][1]) for i in range(len(random_num))]
+    for i in range(1000):
+        cost_tmp = costf(random_input)
+        if best_cost > cost_tmp:
+            best_cost = cost_tmp
+            best_r = random_input
+    return best_cost, best_r
+
+
+def hill_climb(random_num, costf):
+    random_input = [random.randint(random_num[i][0], random_num[i][1]) for i in range(len(random_num))]
+    cycle_time = 0
+    while 1:
+        neighbour = []
+        for j in range(len(random_input)):
+            if random_input[j] > random_num[j][0]:
+                neighbour.append(random_input[0:j] + [random_input[j] - 1] + random_input[j + 1:])
+            if random_input[j] < random_num[j][1]:
+                neighbour.append(random_input[0:j] + [random_input[j] + 1] + random_input[j + 1:])
+        current_cost = costf(random_input)
+        best = current_cost
+        for k in range(len(neighbour)):
+            cost = costf(neighbour[k])
+            if cost < best:
+                best = cost
+                random_input = neighbour[k]
+        cycle_time += 1
+        if current_cost == best: break
+    print(cycle_time)
+    return random_input, best
+
+
+def annealing_optimize(domain, costf, T=1000, cool=0.95, step=1):
+    vec_input = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
+
+    while T > 0.1:
+        i = random.randint(0, len(domain) - 1)
+
+        dir = random.randint(-step, step)
+        vec_tmp = vec_input[:]
+        vec_tmp[i] += dir
+
+        # 确保调整后的数据不超出范围
+        if vec_tmp[i] < domain[i][0]:
+            vec_tmp[i] = domain[i][0]
+        elif vec_tmp[i] > domain[i][1]:
+            vec_tmp[i] = domain[i][1]
+
+        ea = costf(vec_input)
+        eb = costf(vec_tmp)
+        if (eb < ea or random.random() < pow(math.e, -(eb - ea) / T)):
+            vec_input = vec_tmp
+        T = T * cool
+    return vec_input
+
+
+def genetic_optimize(domain, cosf, popsize=50, step=1, mutprob=0.2, elite=0.2, maxtier=100):
+    # 变异操作
+    def mutate(vec):
+        i = random.randint(0, len(domain) - 1)
+        if (random.random() < 0.5 and vec[i] > domain[i][0]):
+            return vec[0:i] + [vec[i] + step] + vec[i + 1:]
+        else:
+            return vec[0:i] + [vec[i] - step] + vec[i + 1:]
+
+    # 交差操作
+    def corss_over(r1, r2):
+        i = random.randint(0, len(domain))
+        return r1[0:i] + r2[i:]
+
+    # 构造初始群
+
+    pop = []
+    for i in range(maxtier):
+        vec = [random.randint(domain[1][0], domain[1][1]) for i in range(len(domain))]
+        pop.append(vec)
+    print(pop)
+    # 每一代中胜出的数量
+    top_elite = int(elite * popsize)
+    for i in range(maxtier):
+        scores = [(cosf(s), s) for s in pop]
+        scores.sort()
+        ranked = [value for (key, value) in scores]#####使用起来非常非常灵活，太赞了！！！！！！！！！！！！
+        pop = ranked[0: top_elite]
+
+        while len(pop) < popsize:
+            if random.random() < mutprob:
+                c = random.randint(0, top_elite)
+                pop.append(mutate(ranked[c]))
+            else:
+                c1 = random.randint(0, top_elite)
+                c2 = random.randint(0, top_elite)
+                pop.append(corss_over(ranked[c1], ranked[c2]))
+        print(scores[0][0])
+    return scores[0][1]
+
+
+random_num = [(0, 9)] * len(people) * 2
+
+best_cost, best_r = random_optimize(random_num, schedulecost)
+print(best_cost)
+print_schedule(best_r)
+print('###########################################################################################')
+
+random_input, best_cost = hill_climb(random_num, schedulecost)
+print(random_input)
+print_schedule(random_input)
+print(best_cost)
+print('###########################################################################################')
+
+vec_input = annealing_optimize(random_num, schedulecost)
+print(vec_input)
+print_schedule(vec_input)
+print('###########################################################################################')
+
+vec_input_2 = genetic_optimize(random_num, schedulecost)
+print(vec_input_2)
+print_schedule(vec_input_2)
+
+
+
+
+
+# r = [1, 4, 3, 2, 7, 3, 6, 3, 2, 4, 5, 3]
+#
+# print_schedule(r)
+# total_price = schedulecost(r)
+# print(total_price)
+# print(schedulecost(r))
+# for key in flight:
+#     print('%s:%s' %(key,flight[key]))
+#     print('\n')
--- search_engine.py
+++ search_engine.py
@@ -1,82 +1,111 @@
 # write on 2016/06/13 by SunChuan
 import urllib.request
-from bs4 import BeautifulSoup
 from urllib.parse import *
 
+from bs4 import BeautifulSoup
+from sqlite3 import dbapi2 as sqlit
+
+ignore_words = set(['the', 'to', 'of', 'and', 'is', 'it', 'in', 'a'])
+
 class crawler:
-    #类初始化
+    # 类初始化
     def __init__(self, dbname):
-        pass
+        self.con = sqlit.connect(dbname)
 
     def __del__(self):
-        pass
-    #数据库连接
+        self.con.close()
+
+    # 数据库连接
     def dbcommit(self):
-        pass
+        self.con.commit()
 
-    #辅助函数，用于获取用户的条目id，如果条目不存在，则将其加入数据库中
-    def get_entry_id(self,table, field, value, creat_new = True):
+    # 辅助函数，用于获取用户的条目id，如果条目不存在，则将其加入数据库中
+    def get_entry_id(self, table, field, value, creat_new=True):
         return None
 
-    #给每个网页添加索引
+    # 给每个网页添加索引
     def add_index(self, url, soup):
-        print('index %s' %url)
+        print('index %s' % url)
 
-    #从一个html提取文字
+    # 从一个html提取文字
 
-    def get_txt_only(self,soup):
-        return None
+    def get_txt_only(self, soup):
+        v = soup.string()
+        if v == None:
+            c = soup.contents
+            result_txt = ''
+            for result in c:
+                sub_txt = self.get_txt_only(result)
+                result_txt += sub_txt + '\n'
+            return result_txt
+        else:return v.strip()
 
-    #根据任何非空白字符进行分次处理
-    def separate_words(self,text):
-        return None
 
-    #判断url是否建立过索引
+    # 根据任何非空白字符进行分次处理
+    def separate_words(self, text):
+        splitter = re.compile('\\W*')#没有理解这个正则表达式的含义
+        return [s.lower() for s in splitter.split(text) if s != '']
+    # 判断url是否建立过索引
     def is_index(self, url):
         return None
 
-    #添加一个关联两个网页的链接
+    # 添加一个关联两个网页的链接
     def add_link_ref(self, url_from, url_to, link_text):
         pass
 
-    #从一个小组网页开始进行广度优先搜索，直至某一给定深度
-    #期间为网页建立索引
-    def crawl(self,pages, depth = 2):
-        ignore_words = set(['the', 'to', 'of', 'and','is', 'it', 'in', 'a'])
+    # 从一个小组网页开始进行广度优先搜索，直至某一给定深度
+    # 期间为网页建立索引
+    def crawl(self, pages, depth=2):
         for i in range(depth):
             new_page = set()
             for page in pages:
                 try:
                     c = urllib.request.urlopen(page)
                 except:
-                    print("Can't open %s" %page)
+                    print("Can't open %s" % page)
                     continue
-                soup = BeautifulSoup(c.read(), 'lxml')
-                self.add_index(page, soup)  #注意这里面调用了类的对象方法，使用self.add_index()而不是调用类的私有方法add_index()
+                soup = BeautifulSoup(c.read(), 'lxml',from_encoding='gb18030')  # 解决编码乱码的问题,编码问题是个很头痛的问题
+                # with open('D:/DataMining/machinelearninginaction/test.txt', 'w', encoding='utf-8') as file:
+                #     file.write(str(soup))
+                # print(soup)
+                self.add_index(page, soup)  # 注意这里面调用了类的对象方法，使用self.add_index()而不是调用类的私有方法add_index()
                 links = soup('a')
                 for link in links:
-                    if('href' in dict(link.attrs)):
+                    if ('href' in dict(link.attrs)):
                         url = urljoin(page, link['href'])
-                        if url.find("'")!= -1:continue
+                        if url.find("'") != -1: continue
                         url = url.split('#')[0]
-                        if url[0:4]=='http' and not self.is_index(url):
+                        if url[0:4] == 'http' and not self.is_index(url):
                             new_page.add(url)
                         link_text = self.get_txt_only(link)
                         self.add_link_ref(page, url, link_text)
-            self.dbcommit()
-        pages = new_page
+                self.dbcommit()
+            pages = new_page
 
 
-
-        pass
-
-    #创建数据库表
+    # 创建数据库表
     def create_table_index(self):
-        pass
+        self.con.execute('create table urllist(url)')
+        self.con.execute('create table wordlist(word)')
+        self.con.execute('create table wordlocation(urlid, wordid, location)')
+        self.con.execute('create table link(fromid integer, toid integer)')
+        self.con.execute('create table linkwords(wordid, linkid)')
+        self.con.execute('create index wordidx on wordlist(word)')
+        self.con.execute('create index urlidx on urllist(url)')
+        self.con.execute('create index wordurlidx on wordlocation(wordid)')
+        self.con.execute('create index urltoidx on link(toid)')
+        self.con.execute('create index urlfromidx on link(fromid)')
 
 
 
-page_list = ['http://news.sina.com.cn/c/nd/2016-06-14/doc-ifxszmaa2015903.shtml']
+
+
+
+
+page_list = ['http://news.163.com/16/0620/22/BQ1ODV1J000156PO.html']
 my_crawler = crawler('my_db')
-my_crawler.crawl(page_list)
+# my_crawler.crawl(page_list)
+my_crawler.create_table_index()
 
+
+#停留在书中P83
